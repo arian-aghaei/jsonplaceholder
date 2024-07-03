98f33bd4e04189faa1fb94a783b33179f2cd0fef
Arian <arianaghaee3@gmail.com>
2024-07-03 16:04:16 +0330

    table for users and localstorage

--- main.js
+++ main.js
@@ -18,53 +18,34 @@ function repeated(number) {
 }
 
 async function load(userid) {
-
+    const stored = localStorage.getItem('posts')
+    if (stored){
+        res = JSON.parse(stored)
+    }
+    else {
+        show('please wait..', 'result')
+        res = await fetchData();
+        localStorage.setItem('posts', JSON.stringify(res));
+    }
     let data = ''
-    show('please wait..')
-    res = await fetchData();
-    // console.log(res)
-    // console.log(res[0])
-    // res.forEach(item => {
-    //     // console.log(item.filter( () => {
-    //     //     if (chk.includes(item.userId)){
-    //     //         return false;
-    //     //     }
-    //     //     chk.push(item.userId);
-    //     //     return true;
-    //     // } ))
-    //
-    //     // console.log(item.userId)
-    //
-    //
-    // })
-    // console.log(res.filter((item) => {
-    //     if (chk.includes(item.userId)) {
-    //         return false;
-    //     }
-    //     chk.push(item.userId);
-    //     return true;
-    // }))
-    console.log(userid);
+    // show('please wait..', 'result')
+    // res = await fetchData();
 
     res.forEach((item) => {
-        if(item.userId === +userid){
-            console.log(item.userId)
-            console.log(item.body)
-            console.log(item.title)
-            data += template(item.userId,item.body, item.title, item.id)
+        if (item.userId === +userid) {
+            data += template(item.userId, item.body, item.title, item.id)
         }
     })
     document.getElementById('getId').value = ''
     console.log(data)
 
     // return document.getElementById("result").innerHTML = data
-    show(data)
+    show(data, 'result')
 }
 
-function show(data){
-    return document.getElementById("result").innerHTML = data
+function show(data, id) {
+    return document.getElementById(id).innerHTML = data
 }
-
 
 
 function template(userid, body, title, id) {
@@ -77,7 +58,7 @@ function template(userid, body, title, id) {
 
                     <div class="justify-content-between row">
                         <div class="d-flex m-3 col">
-                            <button type="button" class="btn btn-outline-secondary" onclick="deleting(${id})">dont show</button>
+                            <button type="button" class="btn btn-outline-secondary" onclick="deleting(${id}, 'posts')">dont show</button>
                         </div>
                         <span class="col-2 my-auto">${userid}</span>
                     </div>
@@ -86,8 +67,75 @@ function template(userid, body, title, id) {
         </div>`;
 }
 
+ids = []
+function deleting(id, row) {
+    if(row === 'users'){
+        ids.push(id)
+        const stored = localStorage.getItem(row)
+        res = JSON.parse(stored)
+        if(res.length){
+            res = res.filter(item => item.id !== id)
+            localStorage.setItem(row, JSON.stringify(res))
+            showTable()
 
-function deleting(id){
-    return document.getElementById(id).classList.add("d-none");
+
+            console.log(stored)
+        }
+    }
+    else {
+        return document.getElementById(id).classList.add("d-none");
+    }
+
 }
 
+
+async function tableData() {
+    let data = await fetch('https://jsonplaceholder.typicode.com/users');
+    let success = await data.json()
+    return success;
+}
+
+async function showTable() {
+    const stored = localStorage.getItem('users')
+    // console.log(stored.length)
+    if (stored){
+        res = JSON.parse(stored)
+    }
+    else {
+        show('please wait..', 'tbody')
+        res = await tableData();
+        ids = []
+        localStorage.setItem('users', JSON.stringify(res));
+    }
+    let data = ''
+
+
+    res.forEach(item => {
+        data += tempTable(item.id, item.name, item.username, item.phone)
+    })
+
+    if (!res.length){
+        localStorage.removeItem('users');
+    }
+    show(data, 'tbody')
+}
+
+
+showTable();
+
+function tempTable(id, name, user, phone) {
+    return `<tr id="${id}" onclick="deleting(${id}, 'users')">
+            <td>${id}</td>
+            <td>${name}</td>
+            <td>${user}</td>
+            <td>${phone}</td>
+        </tr>`
+}
+
+
+
+
+document.getElementById('getId').addEventListener('keyup',function (event) {
+        if (event.key==='Enter')
+            load(this.value);
+})
